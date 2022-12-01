75bdefa93f84270ae076f2680f3bb87c85037f62
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-12-01 13:48:37 +0000

    iteration 1

---
+++ 313_ComboMenu_Gombos.py
@@ -0,0 +1,4 @@
+sandwich = input("What sandwich would you like: Chicken ($5.25), Beef ($6.25), or Tofu ($5.75)")
+#prompts user for which sandwich they want
+print("You picked " + sandwich + ".")
+# tells user which sandwich they picked
