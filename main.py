dd5e2bfd0559966ee4b835792df5e14fce528b8c
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-12-02 13:08:36 +0000

    created 313_ComboMenu_V3_Gombos.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ Max Gombos
 #This file is to allow me to specify the other Python files that run, Not the actual assignment!
 
 #exec(open("313_ComboMenu_Gombos.py").read())
-exec(open("313_ComboMenu_V2_Gombos.py").read())
+#exec(open("313_ComboMenu_V2_Gombos.py").read())
+exec(open("313_ComboMenu_V3_Gombos.py").read())
