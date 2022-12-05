513d99d59b847ec5adfba710d2421ad9f21e55e9
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-12-05 14:29:47 +0000

    cleaned up code, added more comments

--- 313_ComboMenu_V4_Gombos.py
+++ 313_ComboMenu_V4_Gombos.py
@@ -18,20 +18,11 @@ frie_list: list of frie sizes
 frie_price_list: list of frie prices
 type_list: used for finding the combo discount
 """
-print("Welcome!")
-
-
 def welcome():
-  #welcome message
   welcome = 0
   #intializes the welcome variable
-  print(
-    "\nWhat would you like to order?\n  1: Sandwich\n  2: Drink\n  3: French Fries\n  4: Ketchup Packets\n  5: Check Out"
-  )
-  welcome = int(
-    input(
-      "Please enter the number corresponding with the item you would like. (1, 2, 3, 4, or 5)"
-    ))
+  print("\nWhat would you like to order?\n  1: Sandwich\n  2: Drink\n  3: French Fries\n  4: Ketchup Packets\n  5: Check Out")
+  welcome = int(input("Please enter the number corresponding with the item you would like. (1, 2, 3, 4, or 5)"))
   #asks what you would like to add to your order
   #if-elif statement below calls the corresponding function depending on what they want to add to their order
   if (welcome == 1):
@@ -44,20 +35,12 @@ def welcome():
     ketchup()
   elif (welcome == 5):
     check_out()
-
-
 def sandwich():
-  print(
-    "\nChoose between the following sandwiches:\n  1: Chicken ($5.25)\n  2: Beef ($6.25)\n  3: Tofu ($5.75)"
-  )
+  print("\nChoose between the following sandwiches:\n  1: Chicken ($5.25)\n  2: Beef ($6.25)\n  3: Tofu ($5.75)")
   #displays sandwich options
-  index = int(
-    input(
-      "Please select the number of the sandwich you would like to purchase! (1, 2, or 3)"
-    ))
+  index = int(input("Please select the number of the sandwich you would like to purchase! (1, 2, or 3)"))
   #user inputs which sandwich they would like and sets the index variable to it's corresponding number
-  print("You picked " + sandwich_list[index - 1] + " for $" +
-        str(sandwich_price_list[index - 1]) + ".")
+  print("You picked " + sandwich_list[index - 1] + " for $" + str(sandwich_price_list[index - 1]) + ".")
   #tells the user which sandwich they picked and it's price
   final_order_list.append(sandwich_list[index - 1])
   #adds the sandwich to the final order list
@@ -67,10 +50,8 @@ def sandwich():
   #adds a value to the type list signifying that the user added a sandwich to the order
   total_cost()
   #tells the user their order cost so far
-  welcome()
-
-
-  #allows the user to add more items to their order
+  drink()
+  #allows the user to add a drink to their order
 def drink():
   print("\nWould you like to add a drink?\n  1: Yes\n  2: No")
   #asks the user if they want a drink
@@ -78,45 +59,32 @@ def drink():
   #prompts the user to select 1 or 2 if they want a drink or not
   if (wantDrink < 2):
     #checks if they responded yes or no
-    print(
-      "Which size would you like?\n  1: Small ($1)\n  2: Medium ($1.75)\n  3: Large ($2.25)"
-    )
+    print("Which size would you like?\n  1: Small ($1)\n  2: Medium ($1.75)\n  3: Large ($2.25)")
     #if they responded yes, lists the drink size options
-    index = int(
-      input(
-        "Please select the number of the drink size you would like to purchase! (1, 2, or 3)"
-      ))
+    index = int(input("Please select the number of the drink size you would like to purchase! (1, 2, or 3)"))
     #has the user input the size they want, sets the index variable to its corresponding value
-    print("You picked " + drink_list[index - 1] + " for $" +
-          str(drink_price_list[index - 1]) + ".")
+    print("You picked " + drink_list[index - 1] + " for $" + str(drink_price_list[index - 1]) + ".")
     #tells the user the drink size and price they picked
     final_order_list.append(drink_list[index - 1])
-    #adds the drink to the finla order list
+    #adds the drink to the final order list
     total_cost_list.append(drink_price_list[index - 1])
     #adds the drink to the final cost list
-  type_list.append(2)
-  #adds a value to the type list signifying that the user added a drink to the order
-  total_cost()
-  #tells the user their order cost so far
-  welcome()
-
-
-  #allows the user to add more items to thier order
+    type_list.append(2)
+    #adds a value to the type list signifying that the user added a drink to the order
+    total_cost()
+    #tells the user their order cost so far
+    welcome()
+    #allows the user to add more items to their order
+  else:
+    #if the user didn't want a drink:
+    total_cost()
+    #tells the user their order cost so far
+    welcome()
+    #allows the user to add more items to thier order
 def fries():
-  print("\nWould you like to add french fries?\n  1: Yes\n  2: No")
-  #asks the user if they want fries
-  wantFries = int(input("Please select 1 or 2."))
-  #prompts the user to select 1 or 2 if they want fries or not
-  if (wantFries < 2):
-    #checks if they responded yes or no
-    print(
-      "Which size would you like?\n  1: Small ($1)\n  2: Medium ($1.50)\n  3: Large ($2)"
-    )
-    #if they responded yes, lists the frie size options
-    index = int(
-      input(
-        "Please select the number of the frie size you would like to purchase! (1, 2, or 3)"
-      ))
+    print("Which size would you like?\n  1: Small ($1)\n  2: Medium ($1.50)\n  3: Large ($2)")
+    #lists the frie size options
+    index = int(input("Please select the number of the frie size you would like to purchase! (1, 2, or 3)"))
     #has the user input the size they want, sets the index variable to its corresponding value
     if (index == 1):
       # if the user selected a small, askes if they want to mega-size their fries
@@ -125,33 +93,33 @@ def fries():
       if (mega == 1):
         #if they do, sets the local index variable accordingly
         index = 4
-    print("You picked " + frie_list[index - 1] + " for $" +
-          str(frie_price_list[index - 1]) + ".")
+    print("You picked " + frie_list[index - 1] + " for $" + str(frie_price_list[index - 1]) + ".")
     #tells the user the frie size and price they picked
     final_order_list.append(frie_list[index - 1])
     #adds the frie to the finla order list
     total_cost_list.append(frie_price_list[index - 1])
     #adds the frie to the final cost list
-  type_list.append(3)
-  #adds a value to the type list signifying that the user added fries to the order
-  total_cost()
-  #tells the user their order cost so far
-  welcome()
-
-
-  #allows the user to add more items to their order
+    type_list.append(3)
+    #adds a value to the type list signifying that the user added fries to the order
+    total_cost()
+    #tells the user their order cost so far
+    welcome()
+    #allows the user to add more items to their order
 def ketchup():
   print("\n  Ketchup ($0.25 each)")
-  ketchup = int(
-    input("How many ketchup packets would you like? Enter the number here."))
-  print("You added " + str(ketchup) + " ketchup packets for $" +
-        str(ketchup * 0.25) + ".")
+  #tells the user the cost of ketchup
+  ketchup = int(input("How many ketchup packets would you like? Enter the number here."))
+  #asks the user how many ketchup packets they would like, and sets the ketchup local varible to that value
+  print("You added " + str(ketchup) + " ketchup packets for $" + str(ketchup * 0.25) + ".")
+  #tells the user how many ketchup packets they ordered and how much they cost
   total_cost_list.append(ketchup * 0.25)
+  #adds ketchup to the total_cost_list
   final_order_list.append(str(ketchup) + " ketchup packets")
+  #adds ketchup to the final_order_list
   total_cost()
+  #tells user cost of order so far
   welcome()
-
-
+  #allows the user to add more items to order
 def total_cost():
   total_cost = 0
   #sets the total_cost local variable to 0
@@ -176,9 +144,6 @@ def total_cost():
     #updates index variable
   print("Your total cost is $" + str(total_cost))
   #prints total cost
-
-
-#runs each of the functions
 def check_out():
   print("")
   #blank string to add space between parts of the program
@@ -233,4 +198,7 @@ def check_out():
   #prints the order cost, combo discount, and final price
 
 
+print("Welcome!")
+#welcome message
 welcome()
+#runs the welcome function
